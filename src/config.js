9fb6bd171c86e6b36452fdf521eb42245990abd4
Saurabh Salvi <salvisaurabh43@gmail.com>
2020-08-13 11:12:51 -0400

    Setting up our React app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: "us-east-1",
+    BUCKET: "saurabhsalvi-notes-add-uploads"
+  },
+  apiGateway: {
+    REGION: "us-east-1",
+    URL: "https://8a6c3xmekj.execute-api.us-east-1.amazonaws.com/prod"
+  },
+  cognito: {
+    REGION: "us-east-1",
+    USER_POOL_ID: "us-east-1_PDYAbMaGw",
+    APP_CLIENT_ID: "7e3ntd12k3sebfbuc3rr56ojhe",
+    IDENTITY_POOL_ID: "us-east-1:7c7aab79-360c-47e0-ad47-b050cf0cd633"
+  }
+};
